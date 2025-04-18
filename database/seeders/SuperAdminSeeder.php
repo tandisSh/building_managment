353b3e397315d5dc1882f853994678da8854ec08
tandisSh <tandis00shojaee@gmail.com>
2025-04-18 15:39:51 +0330

    db seeders

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $user = User::firstOrCreate([
+            'phone' => '09120000000'
+        ], [
+            'name' => 'Super Admin',
+            'password' => bcrypt('supersecure123'),
+        ]);
+
+        $role = Role::where('name', 'super_admin')->first();
+        $user->roles()->syncWithoutDetaching([$role->id]);
+    }
+}
