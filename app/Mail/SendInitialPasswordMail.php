8ab2e6a0fe92fc62f475f2e19cdefa11f02e409b
tandisSh <tandis00shojaee@gmail.com>
2025-04-29 18:04:55 +0330

    send email for password to users

---
+++ app/Mail/SendInitialPasswordMail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+
+class SendInitialPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $password;
+
+    public function __construct($user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function build()
+    {
+        return $this->subject('رمز عبور اولیه شما')
+            ->view('emails.initial-password');
+    }
+}
+
+
--- app/Services/ResidentService.php
+++ app/Services/ResidentService.php
@@ -5,24 +5,31 @@ namespace App\Services;
 use App\Models\User;
 use App\Models\UnitUser;
 use Illuminate\Support\Facades\Password;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 class ResidentService
 {
+
     public function createResident(array $data, $unitId)
     {
         $user = User::where('phone', $data['phone'])->orWhere('email', $data['email'])->first();
 
         if (!$user) {
+            $randomPassword = Str::random(10); // ساخت رمز ۱۰ رقمی رندوم
+
             $user = User::create([
                 'name' => $data['name'],
                 'phone' => $data['phone'],
                 'email' => $data['email'],
-                'password' => null,
+                'password' => Hash::make($randomPassword),
             ]);
 
-            Password::broker()->sendResetLink(['email' => $user->email]);
+            // ارسال ایمیل رمز عبور
+            Mail::to($user->email)->send(new \App\Mail\SendInitialPasswordMail($user, $randomPassword));
         }
 
+        // ثبت در جدول unit_user
         UnitUser::create([
             'unit_id' => $unitId,
             'user_id' => $user->id,
@@ -31,6 +38,17 @@ class ResidentService
             'to_date' => $data['to_date'],
         ]);
 
+        // ثبت در جدول building_user
+        $unit = \App\Models\Unit::find($unitId);
+
+        if ($unit && $unit->building_id) {
+            \App\Models\BuildingUser::firstOrCreate([
+                'building_id' => $unit->building_id,
+                'user_id' => $user->id,
+            ]);
+        }
+
         return $user;
     }
+
 }
