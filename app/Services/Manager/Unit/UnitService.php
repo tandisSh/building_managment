74c53bc52dd137a7e36a524229f51cd42a19a709
tandisSh <tandis00shojaee@gmail.com>
2025-05-11 23:00:19 +0330

    add UnitService

---
+++ app/Services/Manager/Unit/UnitService.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Services\Manager;
+
+use App\Models\Building;
+use App\Models\Unit;
+
+class UnitService
+{
+    public function getUnitsWithResidents(Building $building)
+    {
+        return $building->units()->with(['users' => function ($query) {
+            $query->wherePivot('role', 'resident');
+        }])->get();
+    }
+
+    public function createUnit(Building $building, array $data): Unit
+    {
+        return $building->units()->create($data);
+    }
+
+    public function updateUnit(Unit $unit, array $data): Unit
+    {
+        $unit->update($data);
+        return $unit;
+    }
+
+    public function deleteUnit(Unit $unit): void
+    {
+        $unit->delete();
+    }
+}
+
