34ea7da875ee9887288f41fbcf63b0420909a6ec
tandisSh <tandis00shojaee@gmail.com>
2025-05-12 00:03:56 +0330

    edit ResidentRequest

---
+++ app/Http/Requests/Resident/ResidentRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Resident;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResidentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        $residentId = $this->route('resident'); 
+
+        return [
+            'unit_id' => 'required|exists:units,id',
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|unique:users,phone,' . $residentId,
+            'email' => 'required|email|max:255|unique:users,email,' . $residentId,
+            'role' => 'required|in:resident,owner',
+            'from_date' => 'required|date',
+            'to_date' => 'nullable|date|after:from_date',
+        ];
+    }
+}
