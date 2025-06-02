33cd0d3b2b3e1789ecaa26a5ee8e8945c0ed21d7
tandisSh <tandis00shojaee@gmail.com>
2025-06-02 17:01:38 +0330

    show invoices

--- app/Http/Controllers/Resident/ResidentInvoiceController.php
+++ app/Http/Controllers/Resident/ResidentInvoiceController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\Http\Controllers\Resident;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invoice;
 use App\Services\Resident\Invoice\InvoiceService;
 use Illuminate\Support\Facades\Auth;
 
@@ -22,12 +24,15 @@ class ResidentInvoiceController extends Controller
         return view('resident.invoices.index', compact('invoices'));
     }
     public function unpaid()
-{
-    $user = Auth::user();
-    $invoices = $this->service->getUserInvoices($user, onlyUnpaid: true);
+    {
+        $user = Auth::user();
+        $invoices = $this->service->getUserInvoices($user, onlyUnpaid: true);
 
-    return view('resident.invoices.unpaid', compact('invoices'));
+        return view('resident.invoices.unpaid', compact('invoices'));
+    }
+    public function show($invoice)
+    {
+        $invoice = Invoice::with('unit')->findOrFail($invoice);
+        return view('manager.invoices.single.show', compact('invoice'));
+    }
 }
-
-}
-
